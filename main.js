2a05f615ac1027f74edd1515005bb26cb6bb4c6f
Francisco Zapata <fran_zapata.13@live.com>
2024-04-13 15:39:11 -0300

    cambios-importantes

---
+++ main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+//Menu
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar-principal');
+
+menu.onclick = () => {
+    menu.classList.toggle('move')
+    navbar.classList.toggle('open-menu')
+}
+/* Menu se cierra y scrollea al enlace */
+window.onscroll = () => {
+    menu.classList.remove('move')
+    navbar.classList.remove('open-menu')
+}
